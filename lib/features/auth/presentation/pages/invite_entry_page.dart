c5f4584d93c7eac62bdbab3d9d7649e3691d3863
BandFlow25 <jason@flowency.co.uk>
2026-03-15 19:54:44 +0000

    chore: bump version to v1.0.2

--- lib/features/auth/presentation/pages/invite_entry_page.dart
+++ lib/features/auth/presentation/pages/invite_entry_page.dart
@@ -225,7 +225,7 @@ class _InviteEntryPageState extends ConsumerState<InviteEntryPage> {
         const SizedBox(height: 16),
         // Version number
         Text(
-          'v1.0.1',
+          'v1.0.2',
           textAlign: TextAlign.center,
           style: theme.textTheme.bodySmall?.copyWith(
                 color: theme.colorScheme.outline,
